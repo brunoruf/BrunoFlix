93e96909ae02249e3214e50619b2d293a8d05022
Bruno Ruf <73960529+brunoruf@users.noreply.github.com>
2022-02-14 23:45:34 -0300

    Add files via upload
    Adicionado Tooltip Actions na Home, corrigido bug no Tooltip de profile, corrigido animação de hide do header

--- _js/index.js
+++ _js/index.js
@@ -1,10 +1,16 @@
+// Tooltip Profile
+
 const tooltipProfile = document.querySelector('.profile-picture')
 tooltipProfile.addEventListener('click', openProfile)
+
 
 function openProfile() {
     document.querySelector('.profile-box').classList.add("active")
     document.querySelector('.profile-picture').classList.add("active")
 }
+
+const stayProfile = document.querySelector('.profile-box')
+stayProfile.addEventListener('mousemove', openProfile)
 
 const tooltipProfileClose = document.querySelector('.profile-box')
 tooltipProfileClose.addEventListener('mouseout', closeProfile)
@@ -14,11 +20,36 @@ function closeProfile() {
     document.querySelector('.profile-picture').classList.remove("active")
 }
 
+
+// Tooltip Plus Actions
+
+const tooltipActionsPlus = document.querySelector('.features-info-actions-plus')
+tooltipActionsPlus.addEventListener('click', openPlusActions)
+
+const staytooltipActionsPlus = document.querySelector('.features-info-actions-plus-box')
+staytooltipActionsPlus.addEventListener('mousemove', openPlusActions)
+
+function openPlusActions() {
+    document.querySelector('.features-info-actions-plus-box').classList.add('active')
+}
+
+const tooltipActionsPlusBoxClose = document.querySelector('.features-info-actions-plus-box')
+tooltipActionsPlusBoxClose.addEventListener('mouseout', closePlusActions)
+function closePlusActions() {
+    document.querySelector('.features-info-actions-plus-box').classList.remove('active')
+}
+
+
+// Header Hide/Show
+
 const header = document.querySelector("header")
 window.onscroll = ()=> {
     if(document.documentElement.scrollTop > 50){
         header.style.visibility = "hidden"
+        header.style.opacity = "0"
+        header.style.transition = ".6s"
     } else{
         header.style.visibility = "visible"
+        header.style.opacity = "1"
     }
 }
