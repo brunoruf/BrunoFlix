a74d83adbc09cb7002a540bcf59e6a86f01d9ea9
Bruno Ruf <brunoruf@hotmail.com>
2022-02-17 14:50:21 -0300

    covers images

--- _js/index.js
+++ _js/index.js
@@ -1,19 +1,19 @@
 // Tooltip Profile
 
 const tooltipProfile = document.querySelector('.profile-picture')
+const stayProfile = document.querySelector('.profile-box')
+const tooltipProfileClose = document.querySelector('.profile-box')
+
+
 tooltipProfile.addEventListener('click', openProfile)
+stayProfile.addEventListener('mousemove', openProfile)
+tooltipProfileClose.addEventListener('mouseout', closeProfile)
 
 
 function openProfile() {
     document.querySelector('.profile-box').classList.add("active")
     document.querySelector('.profile-picture').classList.add("active")
 }
-
-const stayProfile = document.querySelector('.profile-box')
-stayProfile.addEventListener('mousemove', openProfile)
-
-const tooltipProfileClose = document.querySelector('.profile-box')
-tooltipProfileClose.addEventListener('mouseout', closeProfile)
 
 function closeProfile() {
     document.querySelector('.profile-box').classList.remove("active")
@@ -24,17 +24,18 @@ function closeProfile() {
 // Tooltip Plus Actions
 
 const tooltipActionsPlus = document.querySelector('.features-info-actions-plus')
-tooltipActionsPlus.addEventListener('click', openPlusActions)
+const tooltipActionsPlusStay = document.querySelector('.features-info-actions-plus-box')
+const tooltipActionsPlusBoxClose = document.querySelector('.features-info-actions-plus-box')
 
-const staytooltipActionsPlus = document.querySelector('.features-info-actions-plus-box')
-staytooltipActionsPlus.addEventListener('mousemove', openPlusActions)
+tooltipActionsPlus.addEventListener('click', openPlusActions)
+tooltipActionsPlusStay.addEventListener('mousemove', openPlusActions)
+tooltipActionsPlusBoxClose.addEventListener('mouseout', closePlusActions)
+
 
 function openPlusActions() {
     document.querySelector('.features-info-actions-plus-box').classList.add('active')
 }
 
-const tooltipActionsPlusBoxClose = document.querySelector('.features-info-actions-plus-box')
-tooltipActionsPlusBoxClose.addEventListener('mouseout', closePlusActions)
 function closePlusActions() {
     document.querySelector('.features-info-actions-plus-box').classList.remove('active')
 }
@@ -43,13 +44,21 @@ function closePlusActions() {
 // Header Hide/Show
 
 const header = document.querySelector("header")
+const darkBackground = document.querySelector(".main-background")
 window.onscroll = ()=> {
-    if(document.documentElement.scrollTop > 50){
+    
+    if(document.documentElement.scrollTop > 160){
         header.style.visibility = "hidden"
         header.style.opacity = "0"
         header.style.transition = ".6s"
-    } else{
+        darkBackground.style.opacity = "0.4"
+        darkBackground.style.transition= ".6s"
+        darkBackground.style.filter= "blur(1rem)"
+    } else {
         header.style.visibility = "visible"
         header.style.opacity = "1"
+        darkBackground.style.opacity = "1"
+        darkBackground.style.filter= "blur(0)"
     }
 }
+
